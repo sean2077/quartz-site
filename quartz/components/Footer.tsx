1bb877956484f024190fce2d27dd777e4ed5df72
sean2077 <37324769+sean2077@users.noreply.github.com>
2025-12-11 13:23:57 +0800

    fix: 更新页脚中的 Quartz 链接以指向 Sean2077 的 fork 版本

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://github.com/sean2077/quartz-site/">Quartz (Sean2077 fork)</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
